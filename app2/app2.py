fe1733b18bc5ef55961dd62d938df588dc0613ba
luischitala <lrchitala@gmail.com>
2022-08-10 23:45:59 -0500

    finished

---
+++ app2/app2.py
@@ -0,0 +1,12 @@
+from flask import request, Flask
+
+import json
+
+app2 = Flask(__name__)
+
+@app2.route('/')
+def hello_world():
+    return 'Hola, this is App2 :) '
+    
+if __name__ == '__main__':
+    app2.run(debug=True, host='0.0.0.0')
